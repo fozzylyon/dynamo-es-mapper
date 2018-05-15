2733f3a9afcb3e498e29d005ba8eb8b581c923ed
kenneth chatfield <chatfield.kenny@gmail.com>
2018-05-15 12:03:55 -0600

    test index and update api

--- test/index.js
+++ test/index.js
@@ -16,6 +16,9 @@ var input = [{
     NewImage: {
       test: {
         S: 'stuff'
+      },
+      moreStuff: {
+        S: 'more stuff'
       }
     },
     SequenceNumber: '1234',
@@ -23,11 +26,42 @@ var input = [{
     StreamViewType: 'NEW_IMAGE'
   },
   eventSourceARN: 'arn:aws:dynamodb:stuff'
+}, {
+    eventID: 'magic-number',
+    eventName: 'MODIFY',
+    eventVersion: '1.0',
+    eventSource: 'aw:dynamodb',
+    awsRegion: 'region-name',
+    dynamodb: {
+        Keys: {
+            _id: {
+                S: 'TEST_ID'
+            }
+        },
+        NewImage: {
+            test: {
+                S: 'stuff'
+            }
+        },
+        SequenceNumber: '1234',
+        SizeBytes: 12000,
+        StreamViewType: 'NEW_IMAGE'
+    },
+    eventSourceARN: 'arn:aws:dynamodb:stuff'
 }];
 
-var expected = '{\"update\":\"TEST_ID\"}\n{\"doc\":{\"test\":\"stuff\"},\"doc_as_upsert\":true}\n';
+var expected1 = '{\"update\":{\"_id\":\"TEST_ID\"}}\n{\"doc\":{\"test\":\"stuff\",\"moreStuff\":\"more stuff\"},\"doc_as_upsert\":true}\n{\"update\":{\"_id\":\"TEST_ID\"}}\n{\"doc\":{\"test\":\"stuff\"},\"doc_as_upsert\":true}\n';
 
-var result = mapper(input);
-assert.equal(result, expected);
+var result1 = mapper(input);
 
-console.log('Success.  Passed some really rigorous testing');
+assert.equal(result1, expected1);
+
+console.log('Successful test on es update api!!');
+
+var expected2 = '{\"index\":{\"_id\":\"TEST_ID\"}}\n{\"test\":\"stuff\",\"moreStuff\":\"more stuff\"}\n{\"index\":{\"_id\":\"TEST_ID\"}}\n{\"test\":\"stuff\"}\n';
+
+var result2 = mapper(input, '_id', { api: 'index' });
+
+assert.equal(result2, expected2);
+
+console.log('Successful test on es index api!!!');
