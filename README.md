c1afe8f71a87434f068ab66d622dd70e8cfc88cb
kenneth chatfield <chatfield.kenny@gmail.com>
2018-05-15 11:30:21 -0600

    es bulk api options

--- README.md
+++ README.md
@@ -12,3 +12,11 @@ var esActions = dynes(dynamoRecords, 'custom_id');
 
 ...
 ```
+**Options**
+*  `api`: *index, update ( default ) - ES bulk api type*
+```javascript
+var options = {
+    api: 'index' //defaults to 'update'
+};
+var esActions = dynes(dynamoRecords, 'id', options);
+```
--- index.js
+++ index.js
@@ -19,18 +19,26 @@ var _types = {
   REMOVE: 'REMOVE'
 };
 
-module.exports = function(records, key) {
-  key = key || _defaultKey;
+module.exports = function(records, key, options) {
+    key = key || _defaultKey;
 
-  var actions = [];
-  records.forEach(function(rec) {
-    if (rec.eventName === _types.REMOVE) {
-      actions.push(JSON.stringify({ delete: { _id: rec.dynamodb.Keys[key].S } }));
-    }
-    else {
-      actions.push(JSON.stringify({ update: { _id: rec.dynamodb.Keys[key].S} }));
-      actions.push(JSON.stringify({ doc: dynamodbTranslator.translateOutput(rec.dynamodb.NewImage, ItemShape), doc_as_upsert: true }));
-    }
-  });
-  return actions.join('\n') + '\n';
+    options = options || {};
+
+    var actions = [];
+    records.forEach(function(rec) {
+        if (rec.eventName === _types.REMOVE) {
+            actions.push(JSON.stringify({ delete: { _id: rec.dynamodb.Keys[key].S } }));
+        }
+        else {
+            if( options.api === 'index' ) {
+                actions.push(JSON.stringify({ index: { _id: rec.dynamodb.Keys[key].S} }));
+                actions.push(JSON.stringify(dynamodbTranslator.translateOutput(rec.dynamodb.NewImage, ItemShape)));
+            }
+            else {
+                actions.push(JSON.stringify({ update: { _id: rec.dynamodb.Keys[key].S} }));
+                actions.push(JSON.stringify({ doc: dynamodbTranslator.translateOutput(rec.dynamodb.NewImage, ItemShape), doc_as_upsert: true }));
+            }
+        }
+    });
+    return actions.join('\n') + '\n';
 };
